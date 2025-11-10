4853dd45d49a04f19080787d76a06259d0d0df4c
HuyCaoo <144223516+HuyCaoo@users.noreply.github.com>
2025-11-10 13:53:42 +0700

    Add files via upload

---
+++ camera_detect.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("helmetYoloV8_100epochs.pt")
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame)
+    annotated_frame = results[0].plot()
+    cv2.imshow("Helmet Detection", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
